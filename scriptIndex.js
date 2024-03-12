ca5174a241953e1b34746fea1ea31c6628ac59ac
Philipp Müller <philipp.seb.mueller@outlook.de>
2024-03-12 14:15:07 +0100

    addendum

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,11 +1,21 @@
 const contactBtn = document.getElementById('contactBtn');
+const closeBtn = document.getElementById('closeContact');
 const contactInformation = document.querySelector('.contactInformation');
+
+let contactOp = 'none';
 
 contactBtn.addEventListener('click', () => {
     showContact();
-    console.log('check12');
+})
+
+closeBtn.addEventListener('click', () => {
+    closeContact();
 })
 
 function showContact() {
-    contactInformation.style.opacity = 1;
+    contactOp = contactOp === 'none' ? 'block' : 'none';
+    contactInformation.style.display = contactOp;
+}
+function closeContact() {
+    contactInformation.style.display = 'none';
 }
